6b618573e206677936d64ab92a7c250a56f51733
Muhammad A. Fadillah <ed.aliffadillah@gmail.com>
2024-06-08 17:56:05 +0700

    jadi ada high score

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Obstacles : MonoBehaviour
 {
     private GameObject player;
+    public float baseSpeed = 1.0f; // Base speed of the obstacle
+    public float speedMultiplier = 0.3f; // Speed increase per score point
+    private float currentSpeed;
+    private ScoreManager scoreManager; // Reference to the ScoreManager
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        scoreManager = GameObject.FindObjectOfType<ScoreManager>();
+        currentSpeed = baseSpeed;
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreManager != null)
+        {
+            // Adjust the speed based on the score
+            currentSpeed = baseSpeed + scoreManager.GetScore() * speedMultiplier;
+        }
+
+        // Move the obstacle based on the current speed
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,16 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreText;
+    public Text highScoreText; // Tambahkan Text untuk menampilkan high score
     private float score;
+    private int highScore;
+
+    void Start()
+    {
+        // Memuat high score dari PlayerPrefs
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highScoreText.text = "High Score: " + highScore.ToString();
+    }
+
     void Update()
     {
         if (GameObject.FindGameObjectWithTag("Player") != null)
         {
             score += 1 * Time.deltaTime;
-            scoreText.text = ((int)score).ToString();
+            scoreText.text = "Score: " + ((int)score).ToString();
+
+            // Memeriksa apakah skor saat ini melebihi high score
+            if ((int)score > highScore)
+            {
+                highScore = (int)score;
+                highScoreText.text = "High Score: " + highScore.ToString();
+                // Menyimpan high score yang baru
+                PlayerPrefs.SetInt("HighScore", highScore);
+            }
         }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+    }
+
+    public float GetScore()
+    {
+        return score;
     }
 }
